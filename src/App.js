00d853fa8649a23a00e89799410a6a1720860d80
Odie Miranda <odie.miranda@gmail.com>
2021-01-20 19:50:44 +0800

    Fix PrivateRoute handling

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { AuthProvider } from './contexts/AuthContext'
 import PrivateRoute from './components/PrivateRoute'
 
-import PageContainer from './components/PageContainer'
 import LoginPage from './pages/LoginPage'
 import DashboardPage from './pages/DashboardPage'
 
@@ -16,9 +15,7 @@ class App extends React.Component {
         <Router>
           <Switch>
             <Route exact path="/login" component={LoginPage} />
-            <PageContainer>
-              <PrivateRoute exact path="/" component={DashboardPage} />
-            </PageContainer>
+            <PrivateRoute exact path="/" component={DashboardPage} />
           </Switch>
         </Router>
       </AuthProvider>
--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,15 +1,27 @@
 import React from 'react'
 import { Redirect, Route } from 'react-router-dom'
 import { useAuth } from "../contexts/AuthContext"
+import SideNav from './SideNav'
 
 const PrivateRoute = ({ component: RouteComponent, ...rest }) => {
   const { currentUser } = useAuth()
+
+  console.log('PrivateRoute:', currentUser);
 
   return (
     <Route
       {...rest}
       render={props => {
-        return currentUser ? <RouteComponent {...props} /> : <Redirect to="/login" />
+        return (currentUser !== null ? 
+          (<div className="flex row w-screen bg-gray-100 bg-no-repeat bg-top">
+          <div className="bg-white" style={{ width: 400, boxShadow: '2px 0 10px rgba(0,0,0,0.05)' }}>
+            <SideNav />
+          </div>
+          <div className="p-6 w-full min-h-screen relative"><RouteComponent {...props} /></div>
+        </div>)
+           
+          : 
+          <Redirect to="/login" />)
       }}
     />
   )
