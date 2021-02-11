b6909088d2bc53a258c018e0d9cbb2a5577ed286
Jet <jet@localhost.localdomain>
2021-02-11 18:39:49 +0800

    created_at and updated_at for shops

--- functions/src/service/shops.ts
+++ functions/src/service/shops.ts
@@ -3,55 +3,55 @@ import * as admin from 'firebase-admin'
 const db = admin.firestore()
 
 export const getShopsByUserID = async (id) => {
-    return await db
-                  .collection('shops')
-                  .where('user_id', '==', id)
-                  .get()
-                  .then(res => res.docs.map(doc => doc.data()))
+  return await db
+    .collection('shops')
+    .where('user_id', '==', id)
+    .get()
+    .then((res) => res.docs.map((doc) => doc.data()))
 }
 
 export const getShopsByCommunityID = async (id) => {
-    return await db
-                  .collection('shops')
-                  .where('community_id', '==', id)
-                  .get()
-                  .then(res => res.docs.map(doc => doc.data()))
+  return await db
+    .collection('shops')
+    .where('community_id', '==', id)
+    .get()
+    .then((res) => res.docs.map((doc) => doc.data()))
 }
 
 export const getShopByID = async (id) => {
   return await db
-                .collection('shops')
-                .doc(id)
-                .get()
-                .then((res) => {
-                    return res.data()
-                })
+    .collection('shops')
+    .doc(id)
+    .get()
+    .then((res) => {
+      return res.data()
+    })
 }
 
 export const createShop = async (data) => {
   return await db
-                  .collection('shops')
-                  .add(data)
-                  .then((res) => {
-                    return res
-                  })
+    .collection('shops')
+    .add({ ...data, created_at: new Date() })
+    .then((res) => {
+      return res
+    })
 }
 
-export const setShopsStatusOfUser = async (id, status: 'previous' | 'enabled' | 'disabled' | 'archived') => {
-
-  const shopsRef = await db
-                  .collection('shops')
-                  .where('user_id', '==', id)
-                  .get()
+export const setShopsStatusOfUser = async (
+  id,
+  status: 'previous' | 'enabled' | 'disabled' | 'archived'
+) => {
+  const shopsRef = await db.collection('shops').where('user_id', '==', id).get()
 
   const batch = db.batch()
-  shopsRef.forEach(shop => {
+  shopsRef.forEach((shop) => {
     const shopRef = shop.ref
     const shopData = shop.data()
-    const new_status = status === 'previous' ? (shopData.previous_status || 'enabled') : status
+    const new_status = status === 'previous' ? shopData.previous_status || 'enabled' : status
     const updateData: any = {
       status: new_status,
-      previous_status: shopData.status
+      previous_status: shopData.status,
+      updated_at: new Date(),
     }
     if (status === 'archived') updateData.archived_at = new Date()
     batch.update(shopRef, updateData)
--- functions/src/service/users.ts
+++ functions/src/service/users.ts
@@ -3,42 +3,42 @@ import * as admin from 'firebase-admin'
 const db = admin.firestore()
 
 export const getUserByUID = async (uid) => {
-    return await db
-                  .collection('users')
-                  .where('user_uids', 'array-contains', uid)
-                  .get()
-                  .then(res => res.docs.map(doc => doc.data()))
+  return await db
+    .collection('users')
+    .where('user_uids', 'array-contains', uid)
+    .get()
+    .then((res) => res.docs.map((doc) => doc.data()))
 }
 
 export const getUserByID = async (id) => {
   return await db
-                .collection('users')
-                .doc(id)
-                .get()
-                .then((res) => {
-                    return res.data()
-                })
+    .collection('users')
+    .doc(id)
+    .get()
+    .then((res) => {
+      return res.data()
+    })
 }
 
 export const createUser = async (data) => {
   return await db
-                  .collection('users')
-                  .add({ ...data, created_at: new Date() })
-                  .then((res) => {
-                    return res
-                  })
+    .collection('users')
+    .add({ ...data, created_at: new Date() })
+    .then((res) => {
+      return res
+    })
 }
 
 export const updateUser = async (id, data) => {
   return await db
-                  .collection('users')
-                  .doc(id)
-                  .update({ ...data, updated_at: new Date() })
+    .collection('users')
+    .doc(id)
+    .update({ ...data, updated_at: new Date() })
 }
 
 export const archiveUser = async (id) => {
   return await db
-                  .collection('users')
-                  .doc(id)
-                  .update({ status: 'archived', archived_at: new Date(), updated_at: new Date() })
+    .collection('users')
+    .doc(id)
+    .update({ status: 'archived', archived_at: new Date(), updated_at: new Date() })
 }
