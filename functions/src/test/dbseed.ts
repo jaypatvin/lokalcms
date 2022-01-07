b63ab5764b27ced065c5b54291fc2a7a56d34a3c
Jet <jet@DETH.bbrouter>
2022-01-07 20:00:13 +0800

    wip: seeding shops and products

--- functions/src/test/dbseed.ts
+++ functions/src/test/dbseed.ts
@@ -5,26 +5,194 @@ process.env.FIREBASE_AUTH_EMULATOR_HOST = 'localhost:9099'
 admin.initializeApp({ projectId: 'lokal-1baac' })
 
 import Chance from 'chance'
-import { generateCommunityKeywords, generateUserKeywords } from '../utils/generators'
+import {
+  generateCommunityKeywords,
+  generateProductKeywords,
+  generateSchedule,
+  generateShopKeywords,
+  generateUserKeywords,
+} from '../utils/generators'
 import db from '../utils/db'
 import dayjs from 'dayjs'
 import sleep from '../utils/sleep'
+import * as samples from './sampleImages'
 
 const firestoreDb = admin.firestore()
 const auth = admin.auth()
 const chance = new Chance()
 
+const categorySamples = ['drinks', 'fashion', 'food', 'home_goods']
+const sampleSchedules = [
+  {
+    start_time: '09:00 am',
+    end_time: '06:00 pm',
+    start_dates: ['2022-01-03', '2022-01-05', '2022-01-07'],
+    repeat_unit: 1,
+    repeat_type: 'week',
+  },
+  {
+    start_time: '11:00 am',
+    end_time: '10:00 pm',
+    start_dates: ['2022-01-08', '2022-01-09'],
+    repeat_unit: 1,
+    repeat_type: 'week',
+  },
+  {
+    start_time: '08:00 am',
+    end_time: '10:00 pm',
+    start_dates: ['2022-01-03', '2022-01-04', '2022-01-05', '2022-01-06', '2022-01-07'],
+    repeat_unit: 2,
+    repeat_type: 'week',
+  },
+  {
+    start_time: '07:00 am',
+    end_time: '03:00 pm',
+    start_dates: ['2022-01-16'],
+    repeat_unit: 0,
+    repeat_type: 'day',
+  },
+  {
+    start_time: '07:00 am',
+    end_time: '03:00 pm',
+    start_dates: ['2022-01-03'],
+    repeat_unit: 1,
+    repeat_type: 'day',
+  },
+  {
+    start_time: '01:00 pm',
+    end_time: '05:00 pm',
+    start_dates: ['2022-01-05'],
+    repeat_unit: 3,
+    repeat_type: 'day',
+  },
+  {
+    start_time: '09:00 am',
+    end_time: '06:00 pm',
+    start_dates: ['2022-01-05'],
+    repeat_unit: 1,
+    repeat_type: 'month',
+  },
+  {
+    start_time: '09:00 am',
+    end_time: '03:00 pm',
+    start_dates: ['2022-01-07'],
+    repeat_unit: 2,
+    repeat_type: 'month',
+  },
+  {
+    start_time: '06:00 am',
+    end_time: '06:00 pm',
+    start_dates: ['2022-01-09'],
+    repeat_unit: 3,
+    repeat_type: 'month',
+  },
+]
+
+const seedShopsAndProductsOfUser = async ({
+  userId,
+  communityId,
+  displayName,
+}: {
+  userId: string
+  communityId: string
+  displayName: string
+}) => {
+  const shopCount = chance.integer({ min: 0, max: 3 })
+  for (let i = 1; i <= shopCount; i++) {
+    await sleep(100)
+    const name = chance.company()
+    const keywords = generateShopKeywords({ name })
+    const randomSchedule = chance.pickone(sampleSchedules)
+    const { start_time, end_time, start_dates, repeat_unit, repeat_type } = randomSchedule
+    const operatingHours = {
+      start_time,
+      end_time,
+      start_dates,
+      repeat_unit,
+      repeat_type,
+      schedule: generateSchedule({
+        start_time,
+        end_time,
+        start_dates,
+        repeat_unit,
+        repeat_type: repeat_type as any,
+      }),
+    }
+    const { id: shopId } = await firestoreDb.collection('shops').add({
+      name,
+      description: chance.sentence(),
+      user_id: userId,
+      community_id: communityId,
+      is_close: false,
+      status: 'enabled',
+      keywords,
+      archived: false,
+      profile_photo: chance.pickone(samples.shops),
+      cover_photo: chance.pickone(samples.shops),
+      payment_options: [
+        {
+          bank_code: 'bdo',
+          type: 'bank',
+          account_number: chance.cc(),
+          account_name: displayName,
+        },
+        {
+          bank_code: 'gcash',
+          type: 'wallet',
+          account_number: chance.cc(),
+          account_name: displayName,
+        },
+      ],
+      created_at: admin.firestore.Timestamp.now(),
+      operating_hours: operatingHours,
+    })
+
+    const productCount = chance.integer({ min: 2, max: 5 })
+    for (let i = 1; i <= productCount; i++) {
+      await sleep(100)
+      const productName = chance.word({ length: chance.integer({ min: 5, max: 12 }) })
+      const productCategory = chance.pickone(categorySamples)
+      const productKeywords = generateProductKeywords({
+        name: productName,
+        product_category: productCategory,
+      })
+      await firestoreDb.collection('products').add({
+        archived: false,
+        base_price: chance.integer({ min: 10, max: 1000 }),
+        can_subscribe: chance.bool(),
+        community_id: communityId,
+        created_at: admin.firestore.Timestamp.now(),
+        description: chance.sentence(),
+        gallery: [
+          {
+            url: chance.pickone(samples.products),
+            order: 0,
+          },
+        ],
+        keywords: productKeywords,
+        name: productName,
+        product_category: productCategory,
+        quantity: chance.integer({ min: 5, max: 100 }),
+        shop_id: shopId,
+        status: 'enabled',
+        user_id: userId,
+        availability: operatingHours,
+      })
+    }
+  }
+}
+
 const seedCommunities = async () => {
   try {
     for (let i = 1; i <= 3; i++) {
       await sleep(100)
-      const name = chance.company()
-      const subdivision = chance.last({ nationality: 'it' })
+      const name = chance.last({ nationality: 'it' })
+      const subdivision = name
       const city = chance.city()
       const barangay = chance.province({ full: true })
       const state = chance.state()
       const country = chance.country()
-      const zip_code = chance.zip()
+      const zipCode = chance.zip()
       const keywords = generateCommunityKeywords({
         name,
         subdivision,
@@ -32,7 +200,7 @@ const seedCommunities = async () => {
         barangay,
         state,
         country,
-        zip_code,
+        zip_code: zipCode,
       })
       await firestoreDb.collection('community').add({
         name,
@@ -42,14 +210,14 @@ const seedCommunities = async () => {
           country,
           state,
           subdivision,
-          zip_code,
+          zip_code: zipCode,
         },
+        admin: [],
         keywords,
         archived: false,
-        profile_photo:
-          'https://images.unsplash.com/photo-1488521787991-ed7bbaae773c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-        cover_photo:
-          'https://images.unsplash.com/photo-1450609283058-0ec52fa7eac4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+        profile_photo: chance.pickone(samples.communities),
+        cover_photo: chance.pickone(samples.communities),
+        created_at: admin.firestore.Timestamp.now(),
       })
     }
   } catch (error) {
@@ -63,33 +231,33 @@ const seedUsers = async () => {
     await sleep(100)
     try {
       const email = chance.email()
-      const first_name = chance.first()
-      const last_name = chance.last()
-      const display_name = `${first_name} ${last_name}`
-      const admin = chance.bool()
+      const firstName = chance.first()
+      const lastName = chance.last()
+      const displayName = `${firstName} ${lastName}`
+      const isAdmin = chance.bool()
       const community = chance.pickone(communities)
       const keywords = generateUserKeywords({
-        first_name,
-        last_name,
-        display_name,
+        first_name: firstName,
+        last_name: lastName,
+        display_name: displayName,
         email,
       })
       const { uid } = await auth.createUser({
         email,
         emailVerified: true,
         password: 'lokalpassword',
-        displayName: display_name,
+        displayName,
         disabled: false,
       })
-      await firestoreDb.collection('users').add({
+      const { id: userId } = await firestoreDb.collection('users').add({
         user_uids: [uid],
-        first_name,
-        last_name,
-        display_name,
+        first_name: firstName,
+        last_name: lastName,
+        display_name: displayName,
         email,
         roles: {
-          admin,
-          editor: admin || chance.bool(),
+          admin: isAdmin,
+          editor: isAdmin || chance.bool(),
           member: true,
         },
         status: 'active',
@@ -112,11 +280,13 @@ const seedUsers = async () => {
           zip_code: community.address.zip_code,
           country: community.address.country,
         },
-        profile_photo:
-          'https://images.unsplash.com/photo-1543466835-00a7907e9de1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80',
+        profile_photo: chance.pickone(samples.users),
         keywords,
         archived: false,
+        created_at: admin.firestore.Timestamp.now(),
       })
+
+      await seedShopsAndProductsOfUser({ userId, communityId: community.id, displayName })
     } catch (error) {
       console.error('Error creating new user:', error)
     }
---
+++ functions/src/test/sampleImages.ts
@@ -0,0 +1,55 @@
+export const shops = [
+  'https://images.unsplash.com/photo-1507991426709-5bbee2c6a189?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80',
+  'https://images.unsplash.com/photo-1515940175183-6798529cb860?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1629&q=80',
+  'https://images.unsplash.com/photo-1615655406736-b37c4fabf923?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+  'https://images.unsplash.com/photo-1568254183919-78a4f43a2877?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80',
+  'https://images.unsplash.com/photo-1577978924027-8657e5e9cfe3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80',
+  'https://images.unsplash.com/photo-1564470939458-1289338e2d85?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80',
+  'https://images.unsplash.com/photo-1557149559-d74af2d38a1a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+]
+
+export const products = [
+  'https://images.unsplash.com/photo-1569529465841-dfecdab7503b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+  'https://images.unsplash.com/photo-1516535794938-6063878f08cc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80',
+  'https://images.unsplash.com/photo-1599536846247-5ba9230a0b13?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+  'https://images.unsplash.com/photo-1599419685838-28e405ea05b9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+  'https://images.unsplash.com/photo-1511707171634-5f897ff02aa9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80',
+  'https://images.unsplash.com/photo-1557180295-76eee20ae8aa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80',
+  'https://images.unsplash.com/photo-1524805444758-089113d48a6d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80',
+  'https://images.unsplash.com/photo-1508685096489-7aacd43bd3b1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=627&q=80',
+  'https://images.unsplash.com/photo-1546868871-7041f2a55e12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80',
+  'https://images.unsplash.com/photo-1578985545062-69928b1d9587?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1089&q=80',
+  'https://images.unsplash.com/photo-1535141192574-5d4897c12636?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80',
+  'https://images.unsplash.com/photo-1488477304112-4944851de03d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+  'https://images.unsplash.com/photo-1559620192-032c4bc4674e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=729&q=80',
+  'https://images.unsplash.com/photo-1626094309830-abbb0c99da4a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80',
+  'https://images.unsplash.com/photo-1579761314336-f27ea6297ae1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+  'https://images.unsplash.com/photo-1582845512747-e42001c95638?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+  'https://images.unsplash.com/photo-1559715541-5daf8a0296d0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=986&q=80',
+]
+
+export const users = [
+  'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+  'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+  'https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80',
+  'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+  'https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+  'https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+  'https://images.unsplash.com/photo-1524504388940-b1c1722653e1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+  'https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+  'https://images.unsplash.com/photo-1543466835-00a7907e9de1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80',
+  'https://images.unsplash.com/photo-1561037404-61cd46aa615b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+  'https://images.unsplash.com/photo-1517849845537-4d257902454a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80',
+  'https://images.unsplash.com/photo-1588943211346-0908a1fb0b01?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80',
+  'https://images.unsplash.com/photo-1548247416-ec66f4900b2e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80',
+  'https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1143&q=80',
+  'https://images.unsplash.com/photo-1574158622682-e40e69881006?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80',
+]
+
+export const communities = [
+  'https://images.unsplash.com/photo-1522543558187-768b6df7c25c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+  'https://images.unsplash.com/photo-1582213782179-e0d53f98f2ca?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+  'https://images.unsplash.com/photo-1442504028989-ab58b5f29a4a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+  'https://images.unsplash.com/photo-1531844251246-9a1bfaae09fc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1516&q=80',
+  'https://images.unsplash.com/photo-1529209076408-5a115ec9f1c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80',
+]
