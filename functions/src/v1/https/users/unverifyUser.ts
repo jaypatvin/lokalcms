513406a5664cae49a110557ed24e3097008b3fe2
Jet <jet@DETH.bbrouter>
2022-02-04 20:20:19 +0800

    verify ad unverify user endpoint

---
+++ functions/src/v1/https/users/unverifyUser.ts
@@ -0,0 +1,84 @@
+import { Request, Response } from 'express'
+import { UsersService } from '../../../service'
+
+/**
+ * @openapi
+ * /v1/users/{userId}/unverify:
+ *   put:
+ *     tags:
+ *       - users
+ *     security:
+ *       - bearerAuth: []
+ *     description: |
+ *       ### This will unverify an existing user. The user id to be unverified will be acquired from url path
+ *       # Example
+ *       ```
+ *       {
+ *         "notes": "you dont look legit!"
+ *       }
+ *       ```
+ *
+ *     parameters:
+ *       - in: path
+ *         name: userId
+ *         required: true
+ *         description: document id of the user
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               notes:
+ *                 type: string
+ *                 required: true
+ *     responses:
+ *       200:
+ *         description: Updated user
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: ok
+ */
+const unverifyUser = async (req: Request, res: Response) => {
+  const { userId } = req.params
+  const { notes = '', source } = req.body
+  const requestorDocId = res.locals.userDoc.id
+  const roles = res.locals.userRoles
+
+  if (!userId || !requestorDocId) {
+    return res.status(400).json({ status: 'error', message: 'userId is required!' })
+  }
+
+  if (!roles.admin) {
+    return res.status(400).json({
+      status: 'error',
+      message: 'You do not have a permission to verify a user.',
+    })
+  }
+
+  // check if user id is valid
+  const _existing_user = await UsersService.getUserByID(userId)
+  if (!_existing_user) return res.status(400).json({ status: 'error', message: 'Invalid User ID!' })
+
+  const updateData = {
+    updated_by: requestorDocId,
+    updated_from: source || '',
+    'registration.notes': notes,
+    'registration.step': 0,
+    'registration.verified': false,
+  }
+
+  const _result = await UsersService.updateUser(userId, updateData)
+
+  return res.json({ status: 'ok', data: _result })
+}
+
+export default unverifyUser
---
+++ functions/src/v1/https/users/verifyUser.ts
@@ -0,0 +1,86 @@
+import { Request, Response } from 'express'
+import { UsersService } from '../../../service'
+
+/**
+ * @openapi
+ * /v1/users/{userId}/verify:
+ *   put:
+ *     tags:
+ *       - users
+ *     security:
+ *       - bearerAuth: []
+ *     description: |
+ *       ### This will verify an existing user. The user id to be verified will be acquired from url path
+ *       # Example
+ *       ```
+ *       {
+ *         "notes": "looks legit"
+ *       }
+ *       ```
+ *
+ *     parameters:
+ *       - in: path
+ *         name: userId
+ *         required: true
+ *         description: document id of the user
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               notes:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Updated user
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: ok
+ */
+const verifyUser = async (req: Request, res: Response) => {
+  const { userId } = req.params
+  const { notes = '', source } = req.body
+  const requestorDocId = res.locals.userDoc.id
+  const roles = res.locals.userRoles
+
+  if (!userId || !requestorDocId) {
+    return res.status(400).json({ status: 'error', message: 'userId is required!' })
+  }
+
+  if (!roles.admin) {
+    return res.status(400).json({
+      status: 'error',
+      message: 'You do not have a permission to verify a user.',
+    })
+  }
+
+  // check if user id is valid
+  const _existing_user = await UsersService.getUserByID(userId)
+  if (!_existing_user) return res.status(400).json({ status: 'error', message: 'Invalid User ID!' })
+
+  if (_existing_user.registration.step !== 1) {
+    return res.status(400).json({ status: 'error', message: 'Cannot verify user' })
+  }
+
+  const updateData = {
+    updated_by: requestorDocId,
+    updated_from: source || '',
+    'registration.notes': notes,
+    'registration.step': 2,
+    'registration.verified': true,
+  }
+
+  const _result = await UsersService.updateUser(userId, updateData)
+
+  return res.json({ status: 'ok', data: _result })
+}
+
+export default verifyUser
